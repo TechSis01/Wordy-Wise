25b2add3616decdc661e313baecf15f9a22d5c13
TechSis01 <97944210+TechSis01@users.noreply.github.com>
2023-09-08 04:43:49 +0100

    removed console log

--- background.js
+++ background.js
@@ -13,7 +13,6 @@ chrome.contextMenus.create(contextMenuItem)
 
 chrome.contextMenus.onClicked.addListener(function(clickData){
     if(clickData.menuItemId === "wordyWise" && clickData.selectionText){
-        console.log(clickData.selectionText)
         chrome.storage.sync.set({"word":clickData.selectionText})
         
     }
